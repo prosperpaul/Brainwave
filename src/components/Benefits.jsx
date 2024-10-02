e2be1ffcffdd0d30f0f75a8db42bd9de41b5f369
paul <amakapaul4cbn@gmail.com>
2024-10-02 01:49:42 -0700

    again

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -15,7 +15,7 @@ const Benefits = () => {
         <div className="container relative z-2">
            <Heading className='md:max-w-md lg:max-w-2xl' title='Chat Smarter, Not Harder with Brainwave'
            />
-           <div className="flex flex-wrap gap-10 mb-10">
+           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 mb-10">
                {benefits.map((item) => (
                 <div className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]" style={{
                 
